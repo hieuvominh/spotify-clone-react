a4263365583df796e7208ede768b50b9ca146580
hieuvominh <mvohieu@gmail.com>
2022-08-05 00:09:50 +0700

    Implement login function

---
+++ src/components/Spotify.jsx
@@ -0,0 +1,23 @@
+import React from 'react';
+import styled from 'styled-components';
+function Spotify() {
+	return (
+		<Container>
+			<div className="spotify_body">
+				<div className="body">
+					<div className="body_contents">
+						<h1>Spotify</h1>	
+					</div>
+				</div>
+			</div>
+			<div className="spotify_footer">
+
+			</div>
+		</Container>
+	);
+}
+
+export default Spotify;
+
+const Container = styled.div`
+`
